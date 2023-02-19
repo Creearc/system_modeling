6fae0daec674165c6bbc001b359db047df8b286e
Alexandr <knxandr@rambler.ru>
2023-02-19 19:14:27 +0500

    Update lr5_engine.py

--- lr_5_neurons/lr5_engine.py
+++ lr_5_neurons/lr5_engine.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pprint
+import random
 
 
 def read_vectors(file):
@@ -16,8 +17,39 @@ def sign(s):
     return -1 if s < 0 else 1
 
 
+pavel = lambda inp, w : sum([a*b for a,b in zip(inp, w)])
+
+
+class Neuron:
+    def __init__(self, size, c):
+        self.k = [random.randint(-100, 100) / 100 for i in range(size)]
+
+        self.c = c
+
+
+    def run(self, inp: list):
+        return sign(pavel(inp, self.k))
+
+
+    def delta_rule(self, res, y, x):
+        res = self.c * (y - res)
+        for i in range(len(self.k)):
+            self.k[i]+= res * x[i]
+    
+
 if __name__ == '__main__':
     vectors = read_vectors('inp.xls')    
     print(vectors)
 
     C = 0.2
+    neuron = Neuron(3, C)
+    print(neuron.k)
+
+    for iteration in range(20):
+        for i, vector in vectors.items():
+            x, y = vector[:-1], vector[-1]
+            res = neuron.run(x)
+            print(res, y)
+            if res != y:
+                neuron.delta_rule(res, y, x)
+                print(neuron.k)
