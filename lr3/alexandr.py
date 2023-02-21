c795c7d0598e618d13351d9a37ee6075595178b5
Alexandr <knxandr@rambler.ru>
2023-02-21 23:40:16 +0500

    ЗФМУД!

--- lr3/alexandr.py
+++ lr3/alexandr.py
@@ -3,7 +3,7 @@ import id3 as model
 data = model.pd.read_excel('alexandr.xls')
 target = 'y'
 
-answer = model.get_answer(data, list(data.columns), target)
+answer = model.get_answer(data, list(data.columns), target, True)
 print('\n======================')
 print('---Финальное дерево---')
 model.pprint(answer)
